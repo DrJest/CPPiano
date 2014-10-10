0b9aff2fc116fcecfd7f531bf8ef4928e3cd64a7
Simone Albano <drjest@nanos8.pcteor1.mi.infn.it>
2014-10-10 15:38:41 +0200

    Fixed audio freezing

--- audiooutputstreamer.cpp
+++ audiooutputstreamer.cpp
@@ -2,6 +2,7 @@
 
 AudioOutputStreamer::AudioOutputStreamer(int f)
 {
+	// e' il sampling rate 
 	_samplingRate = 44100;
 
 	QAudioFormat format;
@@ -9,14 +10,14 @@ AudioOutputStreamer::AudioOutputStreamer(int f)
 	format.setFrequency(_samplingRate);
 	format.setChannels(1);
 	format.setSampleSize(8);
-	format.setCodec("audio/pcm");
+	format.setCodec("audio/ogg");
 	format.setByteOrder(QAudioFormat::LittleEndian);
 	format.setSampleType(QAudioFormat::SignedInt);
 
 	QAudioDeviceInfo info = QAudioDeviceInfo::defaultInputDevice();
 	if (!info.isFormatSupported(format)) {
-   	qWarning()<<"default format not supported try to use nearest";
-   	format = info.nearestFormat(format);
+	  qWarning()<<"default format not supported try to use nearest";
+   	  format = info.nearestFormat(format);
 	}
 
 	_audio = new QAudioOutput(format, this);
@@ -26,7 +27,7 @@ AudioOutputStreamer::AudioOutputStreamer(int f)
 	_amplitude = 10.;
 	_delta_t = 1/_samplingRate;
 
-  _omega = 2*PI*f;
+    _omega = 2*PI*f;
 
 	_IDWrittenSample = 0;
 	_sizeNolBuffer = 0;
@@ -39,11 +40,10 @@ AudioOutputStreamer::~AudioOutputStreamer()
 
 void AudioOutputStreamer::start()
 {
-  
+ 	QObject::disconnect(_audio, SIGNAL(notify()), this, SLOT(slot_writeMoreData()));
 	QObject::connect(_audio, SIGNAL(notify()), this, SLOT(slot_writeMoreData()));
   
 	_pAudioIOBuffer = _audio->start();
-
 	unsigned int periodSize = _audio->periodSize();
 	_sizeNolBuffer = periodSize;
 	_buffer = (signed char*) calloc(_sizeNolBuffer, sizeof(signed char));	
@@ -55,32 +55,32 @@ void AudioOutputStreamer::start()
 void AudioOutputStreamer::stop()
 {
   _audio->stop();
+  //_audio->reset();
 }
 
 void AudioOutputStreamer::slot_writeMoreData()
 {   
 	int nbBytes = _audio->bytesFree();
-
 	if (nbBytes>0) {
-
-    	if (_sizeNolBuffer<nbBytes) {
+  	if (_sizeNolBuffer<nbBytes) {
         	delete[] _buffer;
         	_sizeNolBuffer = nbBytes;
         	_buffer = (signed char*) calloc(_sizeNolBuffer, sizeof(signed char));
     	}
         	
     	short int value = 0;
-    	for (unsigned int IDSample=0; IDSample<nbBytes; ++IDSample) {
+    	for (int IDSample=0; IDSample<nbBytes; ++IDSample) {
         	float time = (float)_IDWrittenSample * _delta_t;
         	value = (signed char) (_amplitude*(float)(sin(_omega*time)));
         	_buffer[IDSample] = value;
         	++_IDWrittenSample;
     	}
 
-    	int nbWrittenBytes = _pAudioIOBuffer->write((const char*) _buffer, nbBytes);
+    	_pAudioIOBuffer->write((const char*) _buffer, nbBytes);
 
     	if (_IDWrittenSample>_samplingRate) {
         	QObject::disconnect(_audio, SIGNAL(notify()), this, SLOT(slot_writeMoreData()));
+        	_IDWrittenSample = 0;
         	_audio->stop();
     	}
 	}
